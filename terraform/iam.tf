b62675d6a51820a5e92f6199dd146e74be544e15
eren <eren.kilic_25@hotmail.com>
2026-01-14 12:01:18 +0300

    fix: add lambda and lambda_function

--- terraform/iam.tf
+++ terraform/iam.tf
@@ -30,15 +30,20 @@ data "tls_certificate" "github" {
 }
 
 resource "aws_iam_role" "lambda_role" {
-  name = "lambda-metrics-role"
+  name = "lambda_role"
+
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Action = "sts:AssumeRole"
-      Effect = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
-    }]
-  })
+    Statement = [
+      {
+        Action = "sts:AssumeRole"
+        Effect = "Allow"
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+      }
+    ]
+  })  
 }
 
 resource "aws_iam_role_policy" "lambda_role_policy" {
@@ -49,6 +54,17 @@ resource "aws_iam_role_policy" "lambda_role_policy" {
 }
 
 data "aws_iam_policy_document" "lambda_policy_doc" {
+  statement {
+    actions = [
+      "cloudwatch:GetMetricData",
+      "cloudwatch:ListMetrics"
+    ]
+    resources = ["*"]
+  }
+  statement {
+    actions = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.lambda_reports.arn}/*"]
+  }
   statement {
     actions = [
       "cloudwatch:GetMetricData",
--- terraform/lambda.tf
+++ terraform/lambda.tf
@@ -3,7 +3,7 @@ resource "aws_lambda_function" "lambda" {
   filename = data.archive_file.lambda_zip.output_path
   source_code_hash = data.archive_file.lambda_zip.output_base64sha256
 
-  handler = "lambda.function.lambda_handler"
+  handler = "lambda_function.lambda_handler"
   runtime = "python3.9"
   role = aws_iam_role.lambda_role.arn
 }
@@ -13,4 +13,27 @@ data "archive_file" "lambda_zip" {
     type = "zip"
     source_file = "${path.module}/../lambda_function.py" 
     output_path = "${path.module}/../lambda_payload.zip" 
+}
+
+resource "aws_cloudwatch_event_rule" "every_hour" {
+  name = "fargate-metrics-hourly"
+  schedule_expression = "rate(1 hour)"
+}
+
+resource "aws_cloudwatch_event_target" "lambda_target" {
+  rule = aws_cloudwatch_event_rule.every_hour.name
+  target_id = "fargate_metrics_lambda"
+  arn = aws_lambda_function.lambda.arn
+}
+
+resource "aws_lambda_permission" "allow_eventbridge" {
+  statement_id  = "AllowExecutionFromEventBridge"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.lambda.function_name
+  principal     = "events.amazonaws.com"
+  source_arn    = aws_cloudwatch_event_rule.every_hour.arn
+}
+
+resource "aws_s3_bucket" "lambda_reports" {
+  bucket = "s3-b-t-s2"
 }
